42fc75948ecd70ce736aacd4e6039de7ad162da5
Advait Shukla <shukla.advait@gmail.com>
2013-03-31 06:01:06 -0400

    idk

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.getUser);
 app.post('/users', user.createUser);
-app.post('/beers', beers.getBeer);
+app.get('/beers', beers.getBeer);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
--- routes/beers.js
+++ routes/beers.js
@@ -2,20 +2,40 @@
 /*
  * GET beers
  */
-
+var KEY = "7ba3a56166ec88f283a107f9b8f070d8";
 var Mongolian = require('mongolian');
 var db = new Mongolian().db('beerme-db');
-var BrewApi = require('brewerydb-node');
+var Brewery = require('brewery-api-node');
+var brewery = new Brewery(KEY);
 
 exports.getBeer = function(req, res){
 	if(req.query.beer === undefined){
 		res.send({error: "no param provided"});
 	}
 	else {
-		db.collections('beers').find({ $regex: req.query.beer, $options: 'i'}).toArray(function(beers){
+		db.collection('beers').find({ $regex: req.query.beer, $options: 'i'}).toArray(function(beers){
 			if(!beers){
                 //query brewery db with same search
-
+                brewery.searchBeer(req.query.beer, function(err, val){
+                	if(err){
+                		console.log(err);
+                		res.send({error: "problem searching breweryDB"});
+                	}
+                	var valArr = [];
+                	val.forEach(function(value, index){
+                		var info = {
+                			name: value.name,
+                			description: value.description,
+                			style: value.style,
+                			abv: value.abv,
+                			breweries: value.breweries,
+                			label: value.label
+                		}
+                		valArr.push(info);
+                		db.collection('beers').insert(info);
+                	});
+                	res.send(valArr);
+                });
 
 			}
 			else {
