8ea1bf81f33e444d5124ab488deb7d7a26c2747f
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-03-30 23:07:54 -0400

    Add error messages to user creation route

--- routes/user.js
+++ routes/user.js
@@ -10,10 +10,11 @@ exports.list = function(req, res){
 };
 
 exports.createUser = function(req, res) {
-	if(req.query.id === undefined){
-		
-	}
-	else{
-		db.collection('users').insert({id: req.query.id});
-	}
+    if(req.query.id === undefined){
+        res.send("Error: No id specified");
+    }
+    else{
+        db.collection('users').insert({id: req.query.id});
+        res.send("Sucessfully created new user");
+    }
 }
