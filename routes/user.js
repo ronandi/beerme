c4234a425058a85e4bf73e9c665e3d62b2a20b56
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-03-31 01:17:11 -0400

    get rid of database and finish user validation

--- routes/user.js
+++ routes/user.js
@@ -10,11 +10,25 @@ exports.list = function(req, res){
     res.send(req.query);
 };
 
+exports.getUser = function(req, res) {
+    if (req.query.id === undefined) {
+        res.send("Error: No id specified");
+    } else {
+        db.collection('users').findOne({ id: req.query.id }, function(user) {
+            if (!user) {
+                res.send({ error: "User with that id does not exist" });
+            } else {
+                res.send(user);
+            }
+        });
+    }
+}
+
 exports.createUser = function(req, res) {
-    if(req.body.id === undefined){
+    if(req.body.id === undefined) {
         res.send("Error: No id specified");
     }
-    else{
+    else {
         db.collection('users').insert({id: req.body.id});
         res.send("Sucessfully created new user");
     }
