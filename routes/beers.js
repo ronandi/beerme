67f52bb72fa0984f303dbe0b7ae5626e36d19c02
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-03-31 03:35:14 -0400

    use regexes

--- routes/beers.js
+++ routes/beers.js
@@ -5,19 +5,22 @@
 
 var Mongolian = require('mongolian');
 var db = new Mongolian().db('beerme-db');
+var BrewApi = require('brewerydb-node');
 
 exports.getBeer = function(req, res){
 	if(req.query.beer === undefined){
 		res.send({error: "no param provided"});
 	}
-	else{
-		db.collections('beers').findOne({name: req.query.beer}, function(beername){
-			if(!beername){
-				
+	else {
+		db.collections('beers').find({ $regex: req.query.beer, $options: 'i'}).toArray(function(beers){
+			if(!beers){
+                //query brewery db with same search
+
+
 			}
-			else{
+			else {
 				res.send(beername);
 			}
-		})
+		});
 	}
 }
