7495522ca9e11aceb341e70d631e4b34fa6bb495
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-03-30 23:54:22 -0400

    fix db code in user

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,19 @@
  * GET users listing.
  */
 
-var db = require('../database');
+var Mongolian = require('mongolian');
+var db = new Mongolian().db('beerme-db');
 
 exports.list = function(req, res){
     res.send(req.query);
 };
 
 exports.createUser = function(req, res) {
-    if(req.query.id === undefined){
+    if(req.body.id === undefined){
         res.send("Error: No id specified");
     }
     else{
-        db.collection('users').insert({id: req.query.id});
+        db.collection('users').insert({id: req.body.id});
         res.send("Sucessfully created new user");
     }
 }
