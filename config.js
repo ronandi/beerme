e7bb3948bc4b06eb8d25fadd6d95d02a0e0c9699
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-04-02 05:24:39 -0400

    Update config note on API keys in vcs

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 //NOTE: Do not commit this with a key in it!
+//If you choose to put your API key here, DO NOT commit the file to version
+//control! Otherwise, put API key in BREWERY_API environment variable
 var config = {};
 config.breweryApiKey = process.env.BREWERY_API || 'your key here'
 module.exports = config;
