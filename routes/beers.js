6595963d8572fec6efbb120c89a83b7ee9696fcb
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-04-02 05:17:26 -0400

    refactor beers.js.
    - Better (well, mostly) variable names.
    - Break up getBeer method
    - Random bugs I cannot remember...

--- routes/beers.js
+++ routes/beers.js
@@ -7,41 +7,53 @@ var brewery = new Brewery(config.breweryApiKey);
 
 module.exports = function(db) {
     return {
-        getBeer: function(req, res){
+        getBeer: function(req, res) {
             if(req.query.beer === undefined) {
-                res.send({error: "no param provided"});
-            }
-            else {
-                db.collection('beers').find({ $regex: req.query.beer, $options: 'i'}).toArray(function(beers) j{
-                    if(!beers) {
+                res.send({error: "no beer param provided"});
+            } else {
+                //Check if we have the beer in local DB already
+                db.collection('beers').find({ name: { $regex: req.query.beer, $options: 'i'}}, { _id: 0 }).toArray(function(err, beers) {
+                    if(!beers.length > 0) {
+                        console.log("Beer not found in local database");
                         //query brewery db with same search since we dont have
-                        brewery.searchBeer(req.query.beer, function(err, val) {
+                        brewery.searchBeer(req.query.beer, function(err, foundBeers) {
                             if(err) {
-                                console.log(err);
                                 res.send({ error: "problem searching breweryDB" });
+                            } else {
+                                console.log(foundBeers.length + " beers found in breweryDB"); //Possibly more can be found; only ~50 returned by brewery by default...
+                                var simpleFoundBeers = [];
+                                foundBeers.forEach(function(breweryBeerObj) {
+                                    simpleFoundBeers.push(makeSimpleBeer(breweryBeerObj));
+                                });
+                                res.send(simpleFoundBeers);
+                                db.collection('beers').insert(simpleFoundBeers);
                             }
-                            var valArr = [];
-                            val.forEach(function(value, index) {
-                                var info = {
-                                    name: value.name,
-                                    description: value.description,
-                                    style: value.style,
-                                    abv: value.abv,
-                                    breweries: value.breweries,
-                                    label: value.label
-                                }
-                                valArr.push(info);
-                                db.collection('beers').insert(info);
-                            });
-                            res.send(valArr);
                         });
-
-                    }
-                    else {
-                        res.send(beername);
+                    } else {
+                        console.log("Beer found in local database.");
+                        console.log(beers);
+                        res.send(beers);
                     }
                 });
             }
         }
     }
 }
+
+//Make a simpler beer object from that returned by breweryDB
+function makeSimpleBeer(breweryBeer) {
+    var simpleBeer = {
+        name: breweryBeer.name,
+        description: breweryBeer.description,
+        abv: breweryBeer.abv,
+        breweries: breweryBeer.breweries.map(function(brewery) { return brewery.name; }),
+        label: breweryBeer.label
+    }
+    var styletmp;
+    //Not all beers have a category, some have a styleId only
+    if (styletmp = (breweryBeer.style ? breweryBeer.style.category.name : breweryBeer.styleId)) {
+        simpleBeer.style = styletmp;
+    }
+    return simpleBeer;
+}
+
